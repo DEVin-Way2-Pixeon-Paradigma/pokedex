bb9eb15940554c083f2e442fe03119d8a0eb6a1c
Danilo Alves da Silva Sales <danilo.alves@pixeon.com>
2021-12-09 22:14:03 -0300

    pokedex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Pokemon from './components/Pokemon'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1 className="title">Pokedex</h1>
+      
+      <Pokemon 
+        photo="https://i.pinimg.com/originals/96/d2/1d/96d21d738561947f96255e6f12f1754a.png"
+        name="Bulbasaur"
+        hp={100}
+        attack={100}
+        defense={100}
+        types={['grass', 'fire', 'water']}
+      />
+
+    <Pokemon 
+        photo="https://i.pinimg.com/originals/96/d2/1d/96d21d738561947f96255e6f12f1754a.png"
+        name="Charmander"
+        hp={100}
+        attack={100}
+        defense={100}
+        types={['fire', 'fire', 'water']}
+      />
+
+    </>
+  )
 }
 
-export default App;
+export default App
